a23e1b1c436a0395efaa474e3c9091a4177ee913
Tobias Beckhoff <tbeckhof@uni-mainz.de>
2023-12-30 21:41:28 +0100

    Created an initial project.

---
+++ src/main/kotlin/de/tobbecnet/hexaexample/HexaexampleApplication.kt
@@ -0,0 +1,11 @@
+package de.tobbecnet.hexaexample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HexaexampleApplication
+
+fun main(args: Array<String>) {
+	runApplication<HexaexampleApplication>(*args)
+}
---
+++ src/test/kotlin/de/tobbecnet/hexaexample/HexaexampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.tobbecnet.hexaexample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HexaexampleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
