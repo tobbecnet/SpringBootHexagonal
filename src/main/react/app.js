04eab930f8d2b574c0bdb52993e62624cc6f4d75
Tobias B <tobbecnet@gmx.de>
2024-05-13 17:08:45 +0200

    Running simple react example.

--- src/main/react/app.js
+++ src/main/react/app.js
@@ -3,5 +3,28 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 
-// TODO client calls to api?
 
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { meals: [] };
+    }
+
+    componentDidMount() {
+
+    }
+
+    render() {
+        return (
+            <div>
+                <p>React working...</p>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+	<App />,
+	document.getElementById('react')
+)
